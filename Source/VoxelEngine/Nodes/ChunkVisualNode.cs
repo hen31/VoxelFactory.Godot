c578e12e1443a3dc546b987fda3fec20d33b7e46
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2024-01-28 23:01:46 +0100

    Add physics collision shape

--- Source/VoxelEngine/Nodes/ChunkVisualNode.cs
+++ Source/VoxelEngine/Nodes/ChunkVisualNode.cs
@@ -27,7 +27,7 @@ namespace TurtleGames.VoxelEngine
         private void GenerateVisuals(List<VertexWithUv> vertices, List<int> indexes)
         {
             var meshNode = this.GetOrCreate<MeshInstance3D>();
-
+       
             if (vertices.Count == 0)
             {
                 return;
@@ -47,50 +47,12 @@ namespace TurtleGames.VoxelEngine
             surfaceTool.GenerateNormals();
             surfaceTool.Index();
             surfaceTool.GenerateTangents();
-            
-            meshNode.Mesh = surfaceTool.Commit();
+
+            var arrayMesh = surfaceTool.Commit();
+            meshNode.Mesh = arrayMesh;
             surfaceTool.Dispose();
-            /* var indices = indexes.ToArray();
-           var vertexBuffer = Stride.Graphics.Buffer.Vertex.New(GraphicsDevice, vertices.ToArray(),
-                 GraphicsResourceUsage.Default);
 
-             var indexBuffer = Stride.Graphics.Buffer.Index.New(GraphicsDevice, indexes.ToArray());
-
-             var vector3OfSize = new Vector3(ChunkData.Size.X, ChunkData.Height, ChunkData.Size.Y);
-             var points = new[]
-             {
-                 -vector3OfSize,
-                 vector3OfSize,
-             };
-             var boundingBox = BoundingBox.FromPoints(points);
-
-             var customMesh = new Mesh
-             {
-                 Draw = new MeshDraw
-                 {
-
-                     PrimitiveType = PrimitiveType.TriangleList,
-                     DrawCount = indices.Length,
-                     IndexBuffer = new IndexBufferBinding(indexBuffer, true, indices.Length),
-                     VertexBuffers = new[]
-                     {
-                         new VertexBufferBinding(vertexBuffer,
-                             VertexPositionNormalTexture.Layout, vertexBuffer.ElementCount)
-                     },
-                 },
-                 BoundingBox = boundingBox
-
-             };
-
-             model = new Model();
-             // add the mesh to the model
-             model.Meshes.Add(customMesh);
-             model.BoundingBox = boundingBox;
-             meshNode.Model = model;
-
-             model.Materials.Add(Material);
-
-             meshNode.IsShadowCaster = true;*/
+       
         }
 
 
@@ -114,23 +76,12 @@ namespace TurtleGames.VoxelEngine
                 _hasModel = true;
                 var meshNode = this.GetOrCreate<MeshInstance3D>();
                 meshNode.Mesh = _request.Model;
-               // GenerateVisuals(_request.VisualsData.Vertexes, _request.VisualsData.Indexes);
+                var staticBody = this.GetOrCreate<StaticBody3D>();
 
-                /*   var staticColliderComponent = Entity.Get<StaticColliderComponent>();
-                   var colliderShape = new StaticMeshColliderShape(
-                       _request.VisualsData.Vertexes.Select(b => b.Position).ToList(),
-                       _request.VisualsData.Indexes);
-                   if (staticColliderComponent == null)
-                   {
-                       var staticCollider = new StaticColliderComponent();
-                       staticCollider.ColliderShape = colliderShape;
-                       Entity.Add(staticCollider);
-                   }
-                   else
-                   {
-                       staticColliderComponent.ColliderShape = colliderShape;
-                   }*/
-
+                var collisionShape = staticBody.GetOrCreate<CollisionShape3D>();
+                var shape =  new ConcavePolygonShape3D();
+                shape.Data = _request.VisualsData.Indexes.Select(b => _request.VisualsData.Vertexes[b].Position).ToArray();
+                collisionShape.Shape = shape;
                 _request = null;
             }
         }
